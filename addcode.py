55b687d6bcd5546f64f59f6126c1125bed177009
letchap <letchap@users.noreply.github.com>
2023-02-27 08:37:39 +0100

    Update addcode.py

--- addcode.py
+++ addcode.py
@@ -5,37 +5,27 @@ import re
 import codecs
 
 
-# L'expression régulière permettant de trouver le tag dans le texte
+# The regex to find where to add the code. In my pages, it looks like {% addcode content/code/addcode.py %}
 
 CODE_RE = re.compile(r'\{% addcode ?(?P<src>[^\}]*) \%}')
 
 class AddCodePreprocessor(Preprocessor):
 
-# Le pré-processor
-# Je lis les lignes de mon fichier markdown
-# Je les mets une à une dans une liste
-# Dès que je trouve une correspondance à mon expression régulière
-# J'ouvre le fichier source
-# Je le lis ligne à ligne en ajoutant une indentation de 4 espaces pour # le bloc de code markdown
-# Je complète ma liste avec ces lignes
-# Quand j'ai tout lu, je renvoie la liste pour la suite du traitement
-# markdown
-
     def run(self, lines):
         new_lines = [];
-        for line in lines:
+        for line in lines:                                              # I read the file containing the code and add the content to my markdown file
             m = CODE_RE.match(line)
             if m:
                 filein = m.group('src')
                 for line in codecs.open(filein, 'r', encoding="utf-8"):
-                    new_lines.append('    ' + line.rstrip())
+                    new_lines.append('    ' + line.rstrip())            # 4 spaces added to have blockcode in the markdown file
             else:
                 new_lines.append(line)
         return new_lines
 
 
 class AddCodeExtension(Extension):
-# Déclaration de mon extension
+# declare extension (addcode)
     def extendMarkdown(self, md):
         md.preprocessors.register(AddCodePreprocessor(md), 'addcode', 10)
         md.registerExtension(self)
